cb82d716d0a6d40c6c38c460663d743b22e0fa53
PierreChagnon <pc.chagnon@gmail.com>
2024-05-13 16:30:25 +0200

    legal page WIP

--- app/legal/page.jsx
+++ app/legal/page.jsx
@@ -7,7 +7,7 @@ export default function page() {
         <main className='text-white flex flex-col justify-between min-h-dvh relative'>
             <div className='h-full px-8 md:px-20 3xl:px-48 flex flex-col 2xl:items-center gap-4 2xl:gap-16 3xl:text-2xl'>
                 <h1 className={`${bungee.className} my-4 text-center text-2xl 3xl:text-5xl`}>Legal notices</h1>
-                <div className='2xl:w-2/3 mb-4'>
+                {/* <div className='2xl:w-2/3 mb-4'>
                     <h2 className='font-bold mb-2'>Company Identity:</h2>
                     <p><strong>Beyond Games</strong></p>
                     <p><strong>Legal status: </strong>SAS</p>
@@ -26,7 +26,7 @@ export default function page() {
                 <div className='2xl:w-2/3'>
                     <h2 className='font-bold mb-2'>Additional Information:</h2>
                     <p>Beyond Games is a company specialized in developing digital solutions. The website created for the Jean Nicod Institute aims to collect information on preferences in video games for research purposes. The collected data is anonymized and used solely for scientific research.</p>
-                </div>
+                </div> */}
             </div>
             <Footer />
         </main>
