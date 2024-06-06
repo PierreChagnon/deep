1ef2581341ed061a27c77fa1550e5475b17d1830
PierreChagnon <pc.chagnon@gmail.com>
2024-06-06 15:53:07 +0200

    new links + csv added

--- app/research/page.jsx
+++ app/research/page.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 import Footer from '../components/Footer'
 import { bungee } from '../fonts'
 import { FaFileDownload } from "react-icons/fa";
+import { FiExternalLink } from "react-icons/fi";
 import { TbWorld } from "react-icons/tb";
 import Link from 'next/link';
 
@@ -16,39 +17,32 @@ export default function page() {
                     framework for games research developed by Edgar Dubourg and Valérian Chambon
                     at the Institut Jean Nicod, Ecole normale supérieure-PSL.</p>
                 <div className='flex items-center w-full flex-col gap-4 md:flex-row lg:w-2/3 rounded-md bg-white/10 p-6 self-center justify-between'>
-                    <Link target='blank' href="/" className='flex gap-2 items-center cursor-pointer hover:opacity-50 duration-200'>
+                <a href="/data/vg_annotated.csv" download className='flex gap-2 items-center cursor-pointer hover:opacity-50 duration-200'>
                         <span className='text-3xl text-red-400'><FaFileDownload /></span>
-                        <button className='underline'>Download the first paper</button>
-                    </Link>
-                    <p className='md:w-1/2'>This paper provides detailed information and empirical tests on the DEEP model.</p>
-                </div>
-                <div className='flex items-center w-full flex-col gap-4 md:flex-row lg:w-2/3 rounded-md bg-white/10 p-6 self-center justify-between'>
-                    <Link target='blank' href="/" className='flex gap-2 items-center cursor-pointer hover:opacity-50 duration-200'>
-                        <span className='text-3xl text-red-400'><FaFileDownload /></span>
-                        <button className='underline'>Download CSV file</button>
-                    </Link>
-                    <p className='md:w-1/2'>More than 16,000 video games automatically annotated with the DEEP model.</p>
+                        <span className='underline'>Download CSV file</span>
+                    </a>
+                    <p className='md:w-1/2'>More than <strong>16,000 video games</strong> automatically annotated with the DEEP model.</p>
                 </div>
                 <div className='flex items-center w-full flex-col gap-4 md:flex-row lg:w-2/3 rounded-md bg-white/10 p-6 self-center justify-between'>
                     <Link target='blank' href="https://osf.io/z3mvu" className='flex gap-2 items-center cursor-pointer hover:opacity-50 duration-200'>
                         <span className='text-3xl text-red-400'><TbWorld /></span>
-                        <button className='underline'>https://osf.io/z3mvu</button>
+                        <button className='underline flex items-center gap-1'>https://osf.io/z3mvu<FiExternalLink className='text-xl' /></button>
                     </Link>
-                    <p className='md:w-1/2'>Preprint of our paper with theoretical framework and empirical tests.</p>
+                    <p className='md:w-1/2'><strong>Preprint of our paper</strong> with theoretical framework and empirical tests.</p>
                 </div>
                 <div className='flex items-center w-full flex-col gap-4 md:flex-row lg:w-2/3 rounded-md bg-white/10 p-6 self-center justify-between'>
                     <Link target='blank' href="https://osf.io/eyn8u/" className='flex gap-2 items-center cursor-pointer hover:opacity-50 duration-200'>
                         <span className='text-3xl text-red-400'><TbWorld /></span>
-                        <button className='underline'>https://osf.io/eyn8u/</button>
+                        <button className='underline flex items-center gap-1'>https://osf.io/eyn8u/<FiExternalLink className='text-xl' /></button>
                     </Link>
-                    <p className='md:w-1/2'>OSF page where you can find the datasets and R scripts.</p>
+                    <p className='md:w-1/2'>OSF page where you can find <strong>the datasets and R scripts</strong>.</p>
                 </div>
                 <div className='flex items-center w-full flex-col gap-4 md:flex-row lg:w-2/3 rounded-md bg-white/10 p-6 self-center justify-between'>
                     <Link target='blank' href="/" className='flex gap-2 items-center cursor-pointer hover:opacity-50 duration-200'>
                         <span className='text-3xl text-red-400'><TbWorld /></span>
-                        <button className='underline truncate'>frontiersin.org</button>
+                        <button className='underline flex items-center gap-1 truncate'>frontiersin.org<FiExternalLink className='text-xl' /></button>
                     </Link>
-                    <p className='md:w-1/2'>Method article on the automatic annotation method (Frontiers in Artificial Intelligence).</p>
+                    <p className='md:w-1/2'><strong>Method article</strong> on the automatic annotation method (Frontiers in Artificial Intelligence).</p>
                 </div>
                 <p className='mt-12'>As you engage with the DEEP model and its associated resources, we welcome your
                     enquiries and are ready to provide scholarly support. Our team is available to discuss
