ff54b21172b870c0698c23a4aaf6999357b3bfb2
PierreChagnon <pc.chagnon@gmail.com>
2025-04-02 11:20:02 +0200

    unoptimize images

--- app/infos/page.jsx
+++ app/infos/page.jsx
@@ -20,14 +20,14 @@ export default function Results() {
                         <p className='3xl:text-xl '>When it comes to setting goals and thinking about how to achieve them, we all have our unique approaches, which can be very abstract or very concrete. Think about how you set goals in your life. Do you often think about big abstract questions like "<em>What makes a happy life?</em>" or "<em>What's my long-term career plan?</em>" These are <strong>abstract goals</strong>. They're broad, long-term, and often require many actions to achieve. On the other hand, some of us focus more on concrete, day-to-day objectives. Questions such as "<em>What do I need to achieve today?</em>" or "<em>How can I improve my skills in this task?</em>" are examples of <strong>concrete goals</strong>. These goals are more immediate, tangible, and often involve direct actions. In reality, everyone's mind wanders somewhere along this spectrum, from the abstract to the concrete, in the way we represent our goals and think about achieving them.</p>
                     </div>
                     <div className='-z-10 relative w-80 h-80 md:w-1/3 my-10'>
-                        <Image className='object-contain' src='/assets/col.png' alt='column range scheme' fill />
+                        <Image unoptimized className='object-contain' src='/assets/col.png' alt='column range scheme' fill />
                     </div>
                     <div className='flex flex-col md:w-1/3 items-center gap-4'>
                         <h3 className='text-3xl mb-4 font-bold'>Exploration-Exploitation</h3>
                         <p className='3xl:text-xl '>Now, let's look at how we approach achieving these goals, whether they are concrete or abstract. Do you like to explore new ideas, try different things and embrace the unknown? That’s <strong>exploration</strong> – seeking out new experiences, learning new skills or finding new ways of solving problems. Conversely, do you prefer sticking to what you know, hone your existing skills and exploit familiar strategies? This is <strong>exploitation</strong> – optimizing what you already have and making the most of your existing knowledge and skills. In life, as in games, we constantly strike a balance between exploring new possibilities and exploiting known strategies.</p>
                     </div>
                     <div className='-z-10 relative w-80 h-60 md:w-1/3'>
-                        <Image className='object-contain' src='/assets/row.png' alt='row range scheme' fill />
+                        <Image unoptimized className='object-contain' src='/assets/row.png' alt='row range scheme' fill />
                     </div>
                 </div>
                 <span className='w-full my-8 border-b-2 h-[1px]' />
@@ -41,7 +41,7 @@ export default function Results() {
                     <p className='3xl:text-xl lg:w-1/3 lg:px-4'><span className='text-[#B751BA]'>Experimenting</span>: Gamers who score high in Experimenting enjoy hands-on experimentation and learning through direct interaction. They thrive in games like <em>Minecraft</em> or <em>Kerbal Space Program</em>, where trial and error lead to tangible outcomes. A lower score in this dimension may suggest a preference for games with more defined rules or more structured gameplay.</p>
                     <p className='3xl:text-xl lg:w-1/3 lg:px-4'><span className='text-[#FF922A]'>Performing</span>: Gamers who score high in Performing enjoy and excel at games where skill, precision, and efficiency are essential. Titles like <em>Dark Souls</em> and competitive esports games like <em>League of Legends</em> are typical favorites. A lower score on this dimension may indicate a preference for games that focus less on mastery and more on exploration or storytelling.</p>
                     <div className='-z-10 lg:absolute relative h-80 w-full lg:w-full lg:h-full'>
-                        <Image className='object-contain lg:scale-[1.15]' src='/assets/cross.png' alt='cross scheme' fill />
+                        <Image unoptimized className='object-contain lg:scale-[1.15]' src='/assets/cross.png' alt='cross scheme' fill />
                     </div>
                 </div>
                 <span className='w-full my-8 border-b-2 h-[1px]' />
@@ -66,7 +66,7 @@ export default function Results() {
                         <p className='3xl:text-xl '>After collecting the DEEP scores of gamers and their favorite games, we performed a correlation analysis to see how these two sets of scores relate to each other. Think of this analysis as drawing a line on a graph that best fits a scatter of points, where each point represents a gamer. One axis of the graph represents the gamer's DEEP scores and the other the DEEP scores of their favorite games. If gamers who score high on a particular DEEP dimension (such as Expanding) tend to favor games that also score high on that dimension, the points will cluster along the line in a pattern. This pattern – the correlation – is what we were looking for. A strong clustering of points along the line indicates a significant relationship, which shows that <strong>our DEEP model can predict with some effectiveness which games players will enjoy based on their individual gaming styles</strong>.</p>
                     </div>
                     <div className='-z-10 relative lg:w-1/2 w-full h-80 my-10 '>
-                        <Image className='object-contain' src='/assets/graph.png' alt='graph' fill />
+                        <Image unoptimized className='object-contain' src='/assets/graph.png' alt='graph' fill />
                     </div>
                 </div >
             </div >
